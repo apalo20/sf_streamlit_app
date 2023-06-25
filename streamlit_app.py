3c57b914466e9020e3331dcf8527a930e49153bd
apalo20 <137701801+apalo20@users.noreply.github.com>
2023-06-25 19:52:28 +0530

    Update streamlit_app.py
    Added Food Channel details

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('Welcome to My Food Channel')
+streamlit.header('Chicken Pokada')
+streamlit.text('1.Chicken')
+streamlit.text('2.Egg')
+streamlit.text('3.Cornflour')
